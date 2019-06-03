70e56c4870ab01997be308d7b56e9824fde9b723
Son HD <huynhduyson@gmail.com>
2019-06-03 22:46:54 +0700

    add sinleton

---
+++ Singleton/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+class LocationManager{
+    
+    static let shared = LocationManager()
+    
+    var locationGranted: Bool?
+    //Initializer access level change now
+    private init(){}
+    
+    func requestForLocation(){
+        //Code Process
+        locationGranted = true
+        print("Location granted")
+    }
+    
+}
+//Access class function in a single line
+LocationManager.shared.requestForLocation()
